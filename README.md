f4dfffc6177d65024a74b29f2e5e5fc8a874319c
Constance Okoghenun <talktoconstance@yahoo.com>
2014-10-25 17:54:31 +0100

    Added base files for the project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-pi-player
+# pi-player
 =========
+
+Crowd-sourced music player for the Pi
+
+## Quickstart
+
+To get going with started install dependencies with:
+
+
+  * [Install with Bower](http://bower.io): `bower install`
---
+++ js/pi.player.js
@@ -0,0 +1,24 @@
+
+
+//Preprocessing Plugins
+//@prepros-append bower_components/jquery/dist/jquery.js
+//@prepros-append bower_components/jquery.cookie/jquery.cookie.js
+//@prepros-append bower_components/jquery-placeholder/jquery.placeholder.js
+//@prepros-append bower_components/fastclick/lib/fastclick.js
+
+//Foundation plugins
+//@prepros-append bower_components/foundation/js/foundation/foundation.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.topbar.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.reveal.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.dropdown.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.abide.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.alert.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.tab.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.joyride.js
+//@prepros-append bower_components/foundation/js/foundation/foundation.tooltip.js
+
+
+//Other Plugins
+
+// App Declaration
+
